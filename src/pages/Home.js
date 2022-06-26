8437516d1af624202160e6d7bff0414e8e4f1f4d
vc8bp <vc8bp3@gmail.com>
2022-06-26 16:56:22 +0530

    navbar & sidebar done

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import SlideBar from '../components/SlideBar'
+function Home() {
+  return (
+    <>
+        <Navbar/>
+        <SlideBar/>
+    </>
+  )
+}
+
+export default Home
