5e7bffaf54f80b427fadd0d56c1c1c691c468783
vc8bp <vc8bp3@gmail.com>
2022-07-30 19:01:04 +0530

    flex fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import Home from "./pages/Home";
 import {Route, Routes} from "react-router-dom"
+import Navbar from "./components/Navbar";
+import SlideBar from "./components/SlideBar";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home/>}></Route>
-      </Routes>
+      <Navbar/>
+      <Container>
+        <SlideBar/>
+        <Routes>  
+          <Route path="/" element={<Home/>}></Route>
+        </Routes>
+      </Container>
     </>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,23 +6,54 @@ import SlideBar from '../components/SlideBar';
 
 const Container = styled.div`
   display: flex;
+  flex: 5;
+  justify-content: center;
 `
 
 const HomeComponent = styled.div`
-  flex: 5;
-  background-color: red;
   
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  padding: 20px;
 `
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  border: solid teal 1px;
+  border-radius: 1vmax;
+  margin: 20px;
+  padding: 10px 15px;
+`
+
+const Title = styled.h1`
+  font-weight: 500;
+
+`
+const Form = styled.form`
+
+`
+const Input = styled.input`
+
+`
+
+const UsersContainer = styled.div
 
 
 function Home() {
   return (
     <>
-      <NavBar/>
+      
       <Container>
-        <SlideBar/>
         <HomeComponent>
-            <p>Home</p>
+            <Wrapper>
+              <Title>i am title 1</Title>
+
+            </Wrapper>
+            <Wrapper>
+              <Title>i am title 2</Title>
+            </Wrapper>
         </HomeComponent>
       </Container>
     </>
